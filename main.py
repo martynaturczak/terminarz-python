3e58bc84e2bc40a328197bb4502a5aca9d642556
martynaturczak <100962070+martynaturczak@users.noreply.github.com>
2023-03-29 19:14:14 +0200

    restrukturyzacja kodu

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import datetime
-from nicegui import ui
+from nicegui import Client, ui
 
-with ui.column().classes("w-screen h-screen grid grid-rows-2"):
-    with ui.row().classes("w-full h-full"):
-        calendar = ui.date(value = datetime.date.today(), on_change=lambda e: result.set_text(e.value))
-        calendar.classes("w-full h-full")
-        calendar.props('first-day-of-week="1"')
-        result = ui.label()
-    with ui.row().classes("w-full h-full grid grid-cols-2"):
-        with ui.column().classes("w-full h-full"):
-            with ui.card().classes("w-full h-full"):
-                ui.label("tekst 1")
-        with ui.column().classes("w-full h-full"):
-            with ui.card().classes("w-full h-full"):
-                ui.label("tekst 2")
-ui.run()
+@ui.page('/')
+async def main_page(client: Client):
+    await client.connected()
+    await ui.run_javascript('document.getElementById("3").style.padding = "0px"')
+    with ui.column().classes("w-screen h-screen grid grid-rows-2 bg-[#9A5F86]"):
+        with ui.row().classes("w-full h-full"):
+            calendar = ui.date()
+            calendar.classes("w-full h-full")
+            calendar.props('first-day-of-week="1" color=teal-10')
+            calendar.run_method('setToday')
+        with ui.row().classes("w-full h-full grid grid-cols-2"):
+            with ui.column().classes("w-full h-full"):
+                with ui.card().classes("w-full h-full").classes("bg-[#FFFCFE]"):
+                    ui.label("tekst 1")
+            with ui.column().classes("w-full h-full"):
+                with ui.card().classes("w-full h-full").classes("bg-[#FFFCFE]"):
+                    ui.label("tekst 2")
+ui.run(port=1234)
