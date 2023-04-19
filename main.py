23e71f6868092d680543d151aed8359e5bd237ca
martynaturczak <100962070+martynaturczak@users.noreply.github.com>
2023-04-19 22:30:57 +0200

    API do prognoz pogody / panel pogodowy
    dodanie panelu informującego o prognozie pogody aktualnego dnia

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from nicegui import Client, ui
+from nicegui import Client, ui, app
+from weather import Weather
+import datetime
+import requests
+
+
+url = "https://api.open-meteo.com/v1/forecast?latitude=51.10&longitude=17.03&hourly=temperature_2m&hourly=relativehumidity_2m&hourly=cloudcover&hourly=windspeed_10m&hourly=precipitation_probability&&forecast_days=2"
+response = requests.get(url).json()
+
+weather = Weather(response)
+index = weather.index
+
+now = datetime.datetime.now()
+hour = now.strftime("%H")
+minutes = now.strftime("%M")
+day = now.strftime("%A")
+app.add_static_files('/img', 'img')
 
 @ui.page('/')
 async def main_page(client: Client):
     await client.connected()
     await ui.run_javascript('document.getElementById("3").style.padding = "0px"')
-    with ui.column().classes("w-screen h-screen grid grid-rows-2 bg-[#9A5F86]"):
+    with ui.column().classes("w-screen h-screen grid grid-rows-2 bg-[#9A5F86] font-mono"):
         with ui.row().classes("w-full h-full"):
             calendar = ui.date()
             calendar.classes("w-full h-full")
@@ -12,9 +28,27 @@ async def main_page(client: Client):
             calendar.run_method('setToday')
         with ui.row().classes("w-full h-full grid grid-cols-2"):
             with ui.column().classes("w-full h-full"):
-                with ui.card().classes("w-full h-full").classes("bg-[#FFFCFE]"):
+                with ui.card().classes("w-full h-full bg-[#FFFCFE]"):
                     ui.label("tekst 1")
             with ui.column().classes("w-full h-full"):
-                with ui.card().classes("w-full h-full").classes("bg-[#FFFCFE]"):
-                    ui.label("tekst 2")
-ui.run(port=1234)
+                with ui.card().classes("w-full h-full grid grid-rows-2 bg-[#FFFCFE]"):
+                    with ui.row().classes("w-full h-full grid grid-cols-5"):
+                        with ui.column().classes("w-full h-full grid grid-cols-3 col-span-4 gap-0"):
+                            with ui.column().classes("w-full h-full"):
+                                ui.image(weather.img).classes("w-[96px] h-[96px] mt-4 ml-11")
+                            with ui.column().classes("w-full h-full text-5xl flex content-center"):
+                                ui.label(str(weather.temperature[index]) + "°C").classes("pt-8 pr-20")
+                            with ui.column().classes("w-full h-full"):
+                                ui.label("Precipitation probability: " + str(weather.precipitation_probability[index]) + "%")
+                                ui.label("Humidity: " + str(weather.humidity[index]) + "%")
+                                ui.label("Wind speed: " + str(weather.windspeed[index]) + " km/h")
+                        with ui.column().classes("w-full h-full"):
+                            with ui.row().classes("w-full h-3.5"):
+                                ui.label("Weather").classes("w-full text-right text-2xl font-bold")
+                            with ui.row().classes("w-full h-1"):
+                                ui.label(day + ", " + hour + ":" + minutes).classes("w-full text-right")
+                            with ui.row().classes("w-full"):
+                                ui.label(weather.message).classes("w-full text-right")
+                    with ui.row().classes("w-full h-full"):
+                        weather.create_temperature_chart()
+ui.run(port=1236)
---
+++ weather.py
@@ -0,0 +1,71 @@
+from nicegui import ui
+import datetime
+
+class Weather:
+    img = ""
+    message = ""
+    index = 0
+    time = []
+    hours = []
+    temperature = []
+    cloudcover = []
+    humidity = []
+    windspeed = []
+    precipitation_probability = []
+    now = datetime.datetime.now()
+    hour = now.strftime("%H")
+    minutes = now.strftime("%M")
+    day = now.strftime("%A")
+    def __init__(self, weather_data):
+        self.time = weather_data['hourly']['time']
+        self.temperature = weather_data['hourly']['temperature_2m']
+        self.cloudcover = weather_data['hourly']['cloudcover']
+        self.humidity = weather_data['hourly']['relativehumidity_2m']
+        self.windspeed = weather_data['hourly']['windspeed_10m']
+        self.precipitation_probability = weather_data['hourly']['precipitation_probability']
+        self.set_index()
+        self.set_weather_info()
+
+    def set_index(self):
+        now = datetime.datetime.now()
+        hour = now.strftime("%H")
+        minutes = now.strftime("%M")
+        day = now.strftime("%A")
+        date = now.strftime("%Y-%m-%dT%H:00")
+        self.index = self.time.index(date)
+
+    def set_weather_info(self):
+        if self.precipitation_probability[self.index] > 60:
+            self.img = "img/przelotny_deszczyk.png"
+            self.message = "Passing rain"
+        if self.precipitation_probability[self.index] > 80 and self.cloudcover[self.index] > 80 and self.humidity > 50:
+            self.img = "img/deszczyk.png"
+            self.message = "Rainy"
+        if self.temperature[self.index] > 20 and self.cloudcover[self.index] < 50:
+            self.img = "img/sloneczka.png"
+            self.message = "Sunny"
+        if self.temperature[self.index] > 15 and self.cloudcover[self.index] > 20:
+            self.img = "img/chmurki_za_sloneczkiem.png"
+            self.message = "Partially sunny"
+        if self.windspeed[self.index] > 10 and self.humidity[self.index] > 90 and self.precipitation_probability[self.index] > 90:
+            self.img = "img/burza.png"
+            self.message = "Thunderstorm"
+        else:
+            self.img = "img/chmurka.png"
+            self.message = "Cloudy"
+
+    def create_temperature_chart(self):
+        hours = []
+        temperature = []
+        for i in range(0, 24, 3):
+            hours.append((self.time[self.index + i])[-5:])
+            temperature.append(self.temperature[self.index + i])
+        ui.chart({
+                    'title': False,
+                    'chart': {'type': 'column'},
+                    'xAxis': {'categories': hours},
+                    'series': [
+                            {'name': 'Temperature', 'data': temperature}
+                    ],
+                }).classes('w-full h-48')
+
